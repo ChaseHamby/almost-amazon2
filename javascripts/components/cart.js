756a14022e2896d6727e21c00a67e5bdeecc2a9b
Chase Hamby <c.chase.hamby@gmail.com>
2018-10-01 18:17:53 -0500

    got cart and price to print

--- javascripts/components/cart.js
+++ javascripts/components/cart.js
@@ -3,16 +3,19 @@ import {printToDom} from '../helpers/util.js'
 
 const bookStringBuilder = () => {
     let newString = '';
-    for(let i=0; i<book.length;i++){
+    // for(let i=0; i<book.length;i++){
     newString+= `<div class="col-1 offset-md-9">`;
     newString+= `<div class="col offset-md-9 imgContainer">`
-    newString+= `<img class="col" src="https://images-na.ssl-images-amazon.com/images/I/81BM9BDGCUL.jpg" alt="">`;
-    newString+= `<p class="price d-flex justify-content-center" id="price">$${book[0].price}</p>`;
+    newString+= `<img class="col" src="https://images-na.ssl-images-amazon.com/images/I/81BM9BDGCUL.jpg">`;
+    newString+= `<p class="price d-flex justify-content-center" id="price">$${book.price}</p>`;
     newString+= `</div>`
-    newString+= `<img class="cart" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyvtaXUycVf9ZRlWRcL5hLJXKCI43Eb3lbjuOct2uCtw1X055V"></img>`;
+    newString+= `<img class="cartImage" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyvtaXUycVf9ZRlWRcL5hLJXKCI43Eb3lbjuOct2uCtw1X055V" style="width:4em"></img>`;
     newString+= `</div>`;
-    }
     printToDom(newString, 'cart');
 };
 
 export {bookStringBuilder}
+
+// const getPrice = () => {
+//     return book.price;
+// }
--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -4,20 +4,18 @@ import {printToDom} from '../helpers/util.js'
 const book = {
     title: "Falling Up",
     image: "https://images-na.ssl-images-amazon.com/images/I/81BM9BDGCUL.jpg",
-    price: "$15.00",
+    price: "$14.99",
     description: "Cute book full of short stories and poems."
     };
 
 const addCart = document.getElementById('addButton');
 
-let cartStuff = () => {
+const cartStuff = () => {
  addCart.addEventListener('click', function (e) {
     bookStringBuilder();
     })
 };
 
 cartStuff();
-
-
 
 export {book, cartStuff}
